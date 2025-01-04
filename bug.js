286a49fb037469129ec83d9299266943c278294c
BugHunter-X <askzenai@gmail.com>
2025-01-04 03:22:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>This is a Next.js application.</p>
+    </div>
+  );
+}
+```
